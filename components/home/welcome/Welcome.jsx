50ec59f4d61602f3235a79c3897d192e8d5ec871
Ian Barczewski <ibarczewski@gmail.com>
2024-03-28 15:05:24 -0700

    basic ui touchups, basically mock components being rendered

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -13,7 +13,12 @@ import { useRouter } from 'expo-router';
 import styles from './welcome.style';
 import { icons, SIZES } from '../../../constants';
 
+const jobTypes = ['Full-time', 'Part-time', 'Contractor'];
+
 const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time');
+
   return (
     <View style={styles.container}>
       <Text style={styles.userName}>Hello Ian!</Text>
@@ -35,7 +40,23 @@ const Welcome = () => {
         </TouchableOpacity>
       </View>
       <View style={styles.tabsContainer}>
-        <FlatList />
+        <FlatList 
+          horizontal={true}
+          data={jobTypes}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          renderItem={({item}) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}  
+        />
       </View>
     </View>
   )
